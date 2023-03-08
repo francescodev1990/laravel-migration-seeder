7c7a4a8872f40befff0db19ada64d1c20860e38e
Your Name <almannofrancesco@gmail.com>
2023-03-08 16:18:08 +0100

    column added

--- database/migrations/2023_03_08_144734_create_trains_table.php
+++ database/migrations/2023_03_08_144734_create_trains_table.php
@@ -16,6 +16,15 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->text('azienda');
+            $table->text('stazione_di_partenza');
+            $table->text('stazione_di_arrivo');
+            $table->text('orario_di_partenza');
+            $table->text('orario_di_arrivo');
+            $table->text('codice_treno');
+            $table->text('numero_carrozze');
+            $table->text('in_orario');
+            $table->text('cancellato');
         });
     }
 
